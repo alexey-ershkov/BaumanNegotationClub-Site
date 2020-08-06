2d9ddf5347091a63f5ef8a8852c560485d6dc2b5
alexey ershkov <ershkov.alex2000@gmail.com>
2020-08-07 01:01:03 +0300

    now static added and base controller setup

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from main.models import *
+
+
 # Create your views here.
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
